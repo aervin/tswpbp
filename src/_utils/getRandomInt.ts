30f7cf01cbdf4a7c8a63749f5b3accee241ee120
Aaron Ervin <aervin@uste3.com>
2017-12-15 14:32:14 -0500

    restart

--- src/_utils/getRandomInt.ts
+++
@@ -1,4 +0,0 @@
-/* Returns random int between 1 and 6 */
-export default function getRandomDieValue() {
-    return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-}
--- src/_utils/getRandomNumber.ts
+++
@@ -1,3 +0,0 @@
-export default function getRandomNumber(): number {
-    return Math.floor(Math.random() * 100000);
-}
